a32709f01a91f82be2b791d3722f93708ec9902f
Alexandru Mihai <alexandru.mihai_ext@veikkaus.fi>
2026-07-21 13:27:18 +0300

    Initial Commit

---
+++ RandomUsers/RandomUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUsersApp.swift
+//  RandomUsers
+//
+//  Created by Alexandru Mihai on 21/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct RandomUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
