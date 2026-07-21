599fc8f31fc20daf468baa8e05cb61d222f150dd
Alexandru Mihai <alexandru.mihai_ext@veikkaus.fi>
2026-07-21 14:04:06 +0300

    [Task-1] Setup: added packages, gitignore, theme, organized structure

---
+++ RandomUsers/Resources/AppTheme/Theme.swift
@@ -0,0 +1,20 @@
+//
+//  Theme.swift
+//  RandomUsers
+//
+//  Created by Alexandru Mihai on 21/07/2026.
+//
+
+import struct SwiftUI.Color
+
+struct Theme {
+    // MARK: - Main palette
+    
+    static let primaryColor = Color.yellow
+    static let secondaryColor = Color.white
+    static let accentColor = Color.gray
+    
+    // MARK: - Text Colors
+    
+    static let labelColor = Color.black
+}
